51281ffa87eda77f23b1d10d6dfc5f9889a02549
garfdev42 <cepedasan007@gmail.com>
2024-10-03 00:46:37 -0500

    Commit Oct 3

--- assets/js/classes/class.js
+++ assets/js/classes/class.js
@@ -1,4 +1,13 @@
 class Persona {
+  static _conteo = 0;
+  static get conteo() {
+    return Persona._conteo + " Instancias";
+  }
+  static mensaje() {
+    console.log(this.nombre);
+    console.log("Hola a todos, soy un método stático");
+  }
+
   nombre = "";
   codigo = "";
   frase = "";
@@ -12,6 +21,8 @@ class Persona {
     this.nombre = nombre;
     this.codigo = codigo;
     this.frase = frase;
+
+    Persona._conteo++;
   }
 
   set setComidaFavorita(comida) {
@@ -48,5 +59,14 @@ spiderman.miFrase();
 spiderman.setComidaFavorita = "El pie de cereza de la tia May";
 // spiderman.comida = "Duende Verde";
 
-console.log(spiderman.getComidaFavorita);
-console.log(spiderman);
+// console.log(spiderman.getComidaFavorita);
+// console.log(spiderman);
+// Persona._conteo = 2;
+console.log("Conteo Stático:", Persona._conteo);
+console.log(Persona.conteo);
+Persona.mensaje();
+
+Persona.propiedadExterna = "Hola Mundo";
+
+console.log(Persona.propiedadExterna);
+console.log(Persona);
